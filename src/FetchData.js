8bdfbb961d3a365613d4ae8de4b32578fee522ed
Ganesh Murthy <orangewhitegreen@hotmail.com>
2024-07-27 14:18:10 -0400

    displaying header

--- src/FetchData.js
+++ src/FetchData.js
@@ -6,7 +6,9 @@ import Loader from "./Loader";
 function FetchData()
 {
     const[records, setRecords] = useState([]);    
-    const [isDataLoading, setIsDataLoading] = useState([]);
+    const [isDataLoading, setIsDataLoading] = useState(true);
+    const [displayHeader, setDisplayHeader] = useState(false);
+
     const googleAppScriptUrl ='https://script.google.com/macros/s/AKfycbw-28h69fv-Tx-clRfNOv-DwkDgE2kW9EU42nit0OfEC0I0jan4Y1RfM0X423DU3C9mHA/exec';
     
     useEffect(() => { 
@@ -14,11 +16,17 @@ function FetchData()
         .then(response => response.json())
         .then(data => setRecords(data))
         .then(() => setIsDataLoading(false))
+        .then(() => setDisplayHeader(true))
         .catch(err => console.log(err));
     },[])
     
-    return(     
+    return(           
       <>   
+          {
+            displayHeader ? (<div>
+              <h4>Below data is obtained from Google Sheets through AppScripts API</h4>
+            </div>):null 
+          }        
           {
             isDataLoading ? <Loader />: records.map((item, index) => (<Card key={index} announcement={item}/>))
           }
